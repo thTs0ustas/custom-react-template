ba278896c41c8452bee1f218cb2ad36c6b66bcf9
Athanasios Tsoustas <th.ts0ustas@gmail.com>
2022-09-21 16:10:14 +0300

    add webpack loaders (css,images,fonts)

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,18 @@ module.exports = {
 				exclude: /node_modules/,
 				use: ['babel-loader'],
 			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
 		],
 	},
 	resolve: {
